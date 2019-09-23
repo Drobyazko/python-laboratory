21fe4b424f2e24e40a4efb3e96e6efb1c2d7ca8a
Drobyazko <philipjava1111@gmail.com>
2019-09-23 14:12:35 +0300

    Завдання 1

---
+++ laboratory 1/Task 1.py
@@ -0,0 +1,20 @@
+print("Введіть додатє тризначне число")
+a = True
+#while not str(input()).isdigit():
+while a:
+    number = str(input())
+    if (not number.isdigit()):
+        print ("srt is NOT Digit")
+    else:
+        if (not (99<int(number) & int(number)<1000)) :
+            print ("chage number")
+        else:
+            a = False
+
+print ("Str is digit")
+a = int(number[0])
+b = int(number[1])
+c = int(number[2])
+z = int(str(b) + str(c))
+x = 10*z+a
+print(x)
