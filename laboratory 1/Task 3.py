704ee2c0a954a4b8838fba6b598bc448c655a6ce
Drobyazko <philipjava1111@gmail.com>
2019-09-23 14:16:29 +0300

    Завдання 3

---
+++ laboratory 1/Task 3.py
@@ -0,0 +1,19 @@
+print ("Введіть число")
+a = True
+while a:
+    x = str(input())
+    if (not x.isdigit() and not x[0]=="-"):
+        print ("change input value. we need number")
+    elif (x[0]=="-" and x[1:].isdigit):
+        a = False
+    elif (x=="0"):
+        a = False
+    elif (x.isdigit()):
+        a = False
+x = int(x)
+if (x<=2):
+    y=x*x+4*x+5
+    print (y)
+if (x>2):
+    y=1/(x*x+4*x+5)
+    print (y)
