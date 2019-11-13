4da02da6e775d22420b03e732b6c27a3f7057259
Drobyazko <philipjava1111@gmail.com>
2019-11-13 14:12:25 +0200

    Завдання 1

--- laboratory 1/Task 1.py
+++ laboratory 1/Task 1.py
@@ -3,25 +3,35 @@
 Коли результат розділили на 10, а до приватного зліва приписали останню цифру числа x, то вийшло число 237. Знайти число x.
 """""""""""
 print("Введіть додатє тризначне число")
-a = True
-#while not str(input()).isdigit():
-while a:
-    number = str(input())
-    if (not number.isdigit()):
-        print ("srt is NOT Digit")
-    else:
-        if (not (99<int(number) & int(number)<1000)) :
-            print ("chage number")
-        else:
-            a = False
 
-print ("Str is digit")
-a = int(number[0])
-b = int(number[1])
-c = int(number[2])
-z = int(str(b) + str(c))
-x = 10*z+a
-print(x)
+
+# a = True
+# while a:
+#     number = str(input())
+#     if (not number.isdigit()):
+#         print ("srt is NOT Digit")
+#     else:
+#         if (not (99<int(number) & int(number)<1000)) :
+#             print ("chage number")
+#         else:
+#             a = False
+
+
+import re
+def number(x):
+    pattern = "^(\d{3})$"
+    user_input = input()
+    while not re.match(pattern, user_input):
+        user_input = input("Введене значення некоректне , Введіть число:")
+    return number(user_input)
+    print ("Str is digit")
+    a = int(user_input[0])
+    b = int(user_input[1])
+    c = int(user_input[2])
+    z = int(str(b) + str(c))
+    x = 10*z+a
+    return number(input())
+print(number(input()))
 """""""""""
 Програма написана для будь-якого тризначного числа. універсальна
 """""""""""
