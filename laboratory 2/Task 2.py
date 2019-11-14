d668c161009a60219a2de18675ddf6439cb25165
Drobyazko <philipjava1111@gmail.com>
2019-11-14 17:16:01 +0200

    Завдання 2

--- laboratory 2/Task 2.py
+++ laboratory 2/Task 2.py
@@ -1,14 +1,17 @@
 """""""""""
 Скласти програму розкладання натурального числа n на прості множники.
 """""""""""
-i = True
-while (i):
-    my_number = str (input())
-    if (not my_number.isdigit()):
-        print ("change input value")
-    else:
-        my_number = int(my_number)
-        i = False
-for y in range(1, my_number+1):
-    if(my_number%y==0):
-        print(y)
+import re
+def number(num):
+    return bool(re.match('^\d+$',num))
+def number_new():
+    num = input('Enter number: ')
+    while not number(num):
+        num = input('Enter again: ')
+    return main(num)
+def main(num):
+    num = int(num)
+    for y in range(1, num + 1):
+        if(num%y==0):
+            print(y)
+print(number_new())
